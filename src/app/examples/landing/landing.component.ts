8e99d2d4acc324deb6bd67780cc836ecf4e436e5
radyatamaa <43398711+radyatamaa@users.noreply.github.com>
2020-03-16 04:15:38 +0700

    implement detail article and changes footer

--- src/app/examples/landing/landing.component.ts
+++ src/app/examples/landing/landing.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BlogService } from 'app/services/blog-services/blog.service';
+import { User, Article } from 'app/model/blog.model';
 
 @Component({
     selector: 'app-landing',
@@ -9,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 export class LandingComponent implements OnInit {
   focus: any;
   focus1: any;
+  user: User;
+  article: Article;
+  articles: Article;
+  username: string;
+  catrgories: String[] = ['Home', 'Snorkeling', 'Island', 'Travel Tips', 'Travel Planning', '10 Recomended island'];
+  constructor(private blogService: BlogService) { }
 
-  constructor() { }
+  ngOnInit() {
+    
+  this.blogService.GetArticle(1).subscribe(response => {
+    this.article = response;
+    this.get_user(this.article.created_by);
+    this.get_all_articles();
+  })
+  }
 
-  ngOnInit() {}
+  get_user(username){
+    this.blogService.GetUser(username).subscribe(response => {
+      this.user = response;
+  })
+  }
+  get_all_articles(){
+    this.blogService.GetArticles().subscribe(response => {
+      this.articles = response;
+    })
+  }
+  
 
 }
